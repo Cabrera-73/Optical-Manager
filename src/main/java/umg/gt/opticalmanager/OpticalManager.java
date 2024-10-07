b501d466e94444b886dd1e0ba07de004ae1e2a39
Alejandro Cabrera <cabrera888alejandro@gmail.com>
2024-10-07 14:03:43 -0600

    Commit inicial

---
+++ src/main/java/umg/gt/opticalmanager/OpticalManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umg.gt.opticalmanager;
+
+/**
+ *
+ * @author Acer
+ */
+public class OpticalManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
